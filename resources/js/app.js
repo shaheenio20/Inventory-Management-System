95018a85247c683fb1d1ab4880c7e77ce99ba925
shahinur69 <shahinurmubarak238@gmail.com>
2026-06-30 17:43:40 +0600

    used vue.js for the projects

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
+import '../css/app.css';
 
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import Alpine from 'alpinejs';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-window.Alpine = Alpine;
-
-Alpine.start();
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
